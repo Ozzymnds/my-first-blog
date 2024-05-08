78af8ecce1502904f13ce06ff9c36ccbb05c21f1
diego <sanz00zgz@gmail.com>
2024-05-08 17:12:26 +0200

    commit con nuevas funciones como login, delete, etc.

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig', 
 ]
 
 MIDDLEWARE = [
